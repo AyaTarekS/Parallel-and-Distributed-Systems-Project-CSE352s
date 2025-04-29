da7e45489afc27afe987106f69e4f35f72eb78b2
Mohamed Essam <mohamed.modyessam9689@gmail.com>
2025-04-29 04:21:06 +0300

    Website Base

---
+++ public/script.js
@@ -0,0 +1,67 @@
+
+
+
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+  
+    const res = await fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    });
+  
+    const data = await res.json();
+
+    localStorage.setItem('username', username);
+    localStorage.setItem('user_id', data.user_id);
+    document.getElementById("message").textContent = data.message;
+    if (data.message.includes("successful")) {
+      
+      window.location.href = "dashboard.html";
+    } else {
+     
+      document.getElementById("message").textContent = data.message;
+    }
+   
+  
+  });
+
+
+
+  async function loadProfile() {
+    const username = localStorage.getItem('user_id'); 
+    console.log(user_id)
+  
+    if (!user_id) {
+      alert('No user id found. Please log in again.');
+      window.location.href = '/login.html'; 
+      return;
+    }
+  
+    try {
+      const response = await fetch(`/profileInfo?username=${username}`);
+      const profile = await response.json();
+      console.log(profile)
+      if (profile.message) {
+        alert(profile.message);
+      } else {
+        var pic = document.querySelector('#profilePic');
+        if(pic) pic.src = profile.profile_picture || 'defaultPic.jpg';
+        var bal = document.querySelector('#balance');
+        if(bal) bal.textContent = `Balance: $${profile.balance}`;
+        document.querySelector('#bankName').textContent = `Bank: ${profile.bank_name}`;
+       document.querySelector('#subscriptionType').textContent = `Subscription: ${profile.subscription_type}`;
+       document.querySelector('#cardType').textContent = `Card Type: ${profile.card_type}`;
+      }
+    } catch (error) {
+      console.error('Error loading profile:', error);
+    }
+  }
+  
+
+
+
+  
